b8f1c0fa38c681becd2f92e8c6ba66663b3be65d
Mark Chapman <mark.chapman@gmail.com>
2020-01-07 22:48:10 +0000

    Pick up the datepicker options properly.  Support async scenarios where the element has not yet got a parent.

--- fng-bootstrap-date.js
+++ fng-bootstrap-date.js
@@ -1,37 +1,70 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  var uiBootstrapDateModule = angular.module('fng.uiBootstrapDate', ['ui.bootstrap']);
+    var uiBootstrapDateModule = angular.module('fng.uiBootstrapDate', ['ui.bootstrap']);
 
-  uiBootstrapDateModule.controller('FngUIBootstrapDateCtrl', ['$scope', function ($scope) {
-    $scope.popup = {
-      opened: false
-    };
-
-    $scope.open = function() {
-      $scope.popup.opened = true;
-    }
-  }])
-    .directive('fngUiBootstrapDatePicker', ['$compile', '$filter', 'pluginHelper', 'formMarkupHelper', 'cssFrameworkService',
-      function ($compile, $filter, pluginHelper, formMarkupHelper, cssFrameworkService) {
-        return {
-          restrict: 'E',
-          replace: true,
-          priority: 1,
-          controller: 'FngUIBootstrapDateCtrl',
-          link: function (scope, element, attrs) {
-              var template;
-              var processedAttr = pluginHelper.extractFromAttr(attrs, 'fngUiBootstrapDatePicker');
-              template = pluginHelper.buildInputMarkup(scope, attrs.model, processedAttr.info, processedAttr.options, false, false, function (buildingBlocks) {
-                return formMarkupHelper.generateSimpleInput(
-                    buildingBlocks.common + ' uib-datepicker-popup="' + (processedAttr.directiveOptions.format || 'dd/MM/yy') + '" is-open="popup.opened" ng-click="open()" ' + formMarkupHelper.addTextInputMarkup(buildingBlocks, processedAttr.info, ''),
-                    processedAttr.info,
-                    processedAttr.options
-                  );
-              });
-              element.replaceWith($compile(template)(scope));
-            }
+    uiBootstrapDateModule.controller('FngUIBootstrapDateCtrl', ['$scope', function ($scope) {
+        $scope.popup = {
+            opened: false
         };
-      }]
-    )
+
+        $scope.open = function () {
+            $scope.popup.opened = true;
+        }
+    }])
+        .directive('fngUiBootstrapDatePicker', ['$compile', '$filter', '$timeout', 'pluginHelper', 'formMarkupHelper', 'cssFrameworkService',
+            function ($compile, $filter, $timeout, pluginHelper, formMarkupHelper) {
+                return {
+                    restrict: 'E',
+                    replace: true,
+                    priority: 1,
+                    controller: 'FngUIBootstrapDateCtrl',
+                    link: function (scope, element, attrs) {
+                        var template;
+                        var processedAttr = pluginHelper.extractFromAttr(attrs, 'fngUiBootstrapDatePicker');
+                        var overRiddenDefaults = {};
+                        overRiddenDefaults = Object.assign({}, overRiddenDefaults, processedAttr.directiveOptions);
+                        var overRiddenDateDefaults = {
+                            showWeeks: false
+                        };
+                        var jsonDateOptions = {};
+                        if (processedAttr.directiveOptions['date-options']) {
+                            jsonDateOptions = JSON.parse(processedAttr.directiveOptions['date-options'].replace(/'/g, '"'));
+                        }
+                        scope.dateOptions = Object.assign({}, overRiddenDateDefaults, jsonDateOptions);
+
+                        template = pluginHelper.buildInputMarkup(scope, attrs.model, processedAttr.info, processedAttr.options, false, false, function (buildingBlocks) {
+                            let str = '';
+                            for (var opt in overRiddenDefaults) {
+                                if (opt !== 'date-options') {
+                                    str += ' ' + opt + '="' + overRiddenDefaults[opt] + '"';
+                                }
+                            }
+
+                            return formMarkupHelper.generateSimpleInput(
+                                buildingBlocks.common + str + ' uib-datepicker-popup="' + (processedAttr.directiveOptions.format || processedAttr.directiveOptions['date-format'] || 'dd/MM/yy') + '" is-open="popup.opened" ng-click="open()" ' + formMarkupHelper.addTextInputMarkup(buildingBlocks, processedAttr.info, ''),
+                                processedAttr.info,
+                                processedAttr.options
+                            );
+                        });
+                        const html = $compile(template)(scope);
+                        if (element[0].parentElement) {
+                            element.replaceWith(html);
+                        } else {
+
+                            function afterTimeout() {
+                                $timeout(() => {
+                                    if (element[0].parentElement) {
+                                        element.replaceWith(html);
+                                    } else {
+                                        afterTimeout();
+                                    }
+                                });
+                            }
+                            afterTimeout();
+                        }
+                    }
+                };
+            }]
+        )
 })();
